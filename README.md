e650c2aee8224079b7439bbe2db5a4f2d8cf0de6
Prakhar Srivastav <srivprakhar@gmail.com>
2024-01-13 03:16:43 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # server-sent-events
+This repository is used to support this blog post. The interested readers can clone the repository and follow below steps to run the application. The code base has comments at the relevant places to help readers understand the logic.
+
+
+## Running application
+```shell
+# from the project root run
+go run main.go
+```
+
+This starts up an http server on port 8080 . 
+- Open the browser and, navigate to [http://localhost:8080](http://localhost:8080).
+- Click on the big blue button.
+- The server should start streaming the response to browser.
+- Browser will be updated the realtime response.
+
+
+If you have any questions, please create an issue and I will be more than happy to answer the queries.
--- main.go
+++ main.go
@@ -10,20 +10,29 @@ import (
 
 func main() {
 
-	// route to html
+	// serve html content on the path '/'
 	http.Handle("/", http.FileServer(http.Dir("static")))
 
-	// route to sse
+	// serve sse on the path '/events'
 	http.HandleFunc("/events", eventsHandler)
 
-	// serve application
+	// run http server on port 8080
 	http.ListenAndServe(":8080", nil)
 }
 
+// eventsHandler receives the request, sets up the correct response headers
+// and returns 10 cat breeds to the client.
+// On 11th iteration, the control breaks out of the loop and the
+// http connection is dropped.
+// This connection drop is captured by client and client handles the connection drop accordingly
 func eventsHandler(w http.ResponseWriter, r *http.Request) {
+
+	// set up the required response headers
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	// content-type should be 'text/event-stream'
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
+	// keep the connection alive for this response
 	w.Header().Set("Connection", "keep-alive")
 
 	count := 1
@@ -31,16 +40,18 @@ func eventsHandler(w http.ResponseWriter, r *http.Request) {
 
 	for {
 		if count > 10 {
+			// break out of the loop on the 11th iteration, to drop the http connection
 			break
 		}
 
 		breed := gofaker.Cat()
-		fmt.Fprintf(w, "data: %s \n\n", fmt.Sprintf("%d : %s", count, breed))
-		w.(http.Flusher).Flush()
-
+		// sleep for 100ms to simulate server processing
 		time.Sleep(100 * time.Millisecond)
+		fmt.Fprintf(w, "data: %s \n\n", fmt.Sprintf("%d : %s", count, breed))
+
+		// flush the response
+		w.(http.Flusher).Flush()
 
 		count++
 	}
-
 }
